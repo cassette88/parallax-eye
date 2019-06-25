25513c31c0a43cc9dda788647fb76c0a2d9be776
Dave <chopin@iJones.local>
2019-06-25 12:35:47 -0400

    commit with emotion react swipeable and react spring working

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,33 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "@emotion/styled"
+import SwipeableViews from 'react-swipeable-views';
+import { Parallax, ParallaxLayer} from 'react-spring/renderprops-addons.cjs'
+
+const Emoting = styled.h2`
+  color: green;
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SwipeableViews enableMouseEvents resistance>
+   <div><Emoting>Emotions?</Emoting></div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <div>
+      <h3>A Little Bit more to help the transition</h3>
+    </div>
+    <div>
+      <Parallax pages={2}>
+      <ParallaxLayer offset={0}speed={0.5}> 
+      <Emoting>Again in the green</Emoting>
+      </ParallaxLayer>
+      </Parallax>
+    </div>
+    </SwipeableViews>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
