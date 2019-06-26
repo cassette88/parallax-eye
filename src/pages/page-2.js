aa21805f3b68e1dec4c171064b790b918b2d0165
Dave <chopin@iJones.local>
2019-06-25 20:39:27 -0400

    parallax page two experiment

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,14 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PLayers from "../components/Parallax"
 
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+ 
+      <PLayers/>
+   
 )
 
 export default SecondPage
